2245326d3d17084eff7d045a9ffdae114076cec6
Richard Barker <richbarker@gmail.com>
2020-12-21 20:28:39 +1100

    day 1 complete

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -12,11 +12,23 @@
                      (= 2020 sum))]
       product)))
 
+(defn calc-day-1-part-2 [input]
+  (first
+    (for [x input
+          y input
+          z input
+          :let [sum     (+ x y z)
+                product (* x y z)]
+          :when (and (not= x y z)
+                     (= 2020 sum))]
+      product)))
+
 (def input-day1 (map #(Integer/parseInt %)
       (clojure.string/split-lines
         (slurp (io/resource "input-day1")))))
 
 (def answer-1 (calc-day-1 input-day1))
+(def answer-2 (calc-day-1-part-2 input-day1))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/aoc/core_test.clj
+++ test/aoc/core_test.clj
@@ -10,8 +10,20 @@
                         366
                         299
                         675
-                        1456]))))
+                        1456])))
+
+    (is (= 241861950
+           (calc-day-1-part-2 [1721
+                               979
+                               366
+                               299
+                               675
+                               1456]))))
 
   (testing "question 1"
     (is (= 355875
-           (calc-day-1 input-day1)))))
+           (calc-day-1 input-day1))))
+
+  (testing "question 2"
+    (is (= 140379120
+           (calc-day-1-part-2 input-day1)))))
