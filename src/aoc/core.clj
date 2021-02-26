8b2b0eee8004b3334356704dff548c992a0b17a0
Richard Barker <richbarker@gmail.com>
2021-02-27 08:18:25 +1100

    day 2 part 1

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -1,5 +1,6 @@
 (ns aoc.core
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
   (:gen-class))
 
 (defn calc-day-1 [input]
@@ -23,12 +24,39 @@
                      (= 2020 sum))]
       product)))
 
+(defn read-input [filename]
+  (clojure.string/split-lines
+    (slurp (io/resource filename))))
+
 (def input-day1 (map #(Integer/parseInt %)
-      (clojure.string/split-lines
-        (slurp (io/resource "input-day1")))))
+                     (read-input "input-day1")))
 
 (def answer-1 (calc-day-1 input-day1))
 (def answer-2 (calc-day-1-part-2 input-day1))
+
+;; Day 2
+
+(def answer-1 (count
+                (filter
+                  (fn [pwd-policy]
+                    (let [
+                          [character-policy [character _] password] (str/split pwd-policy #" ")
+                          [lwr uppr] (str/split character-policy #"-")
+                          character-count (count (filter #{character} password))
+                          lwr             (Integer/parseInt lwr)
+                          uppr            (Integer/parseInt uppr)]
+
+                      (<= lwr character-count uppr)))
+
+                  (read-input "input-day2"))))
+
+
+(map
+  (fn [pwd-policy]
+    (let [[count [character _] password] (str/split pwd-policy #" ")]
+      [count character password]))
+  (read-input "input-day2"))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
